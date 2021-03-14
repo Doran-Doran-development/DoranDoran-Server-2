60e0129b39a7bd0802ba6e8e54498b63a97727c8
hanbin8269 <hanbin8269@gmail.com>
2021-03-15 08:56:15 +0900

    [ADD] manage.py 에서 dotenv를 환경변수로 추가

--- dorandoran/manage.py
+++ dorandoran/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
@@ -19,4 +19,5 @@ def main():
 
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
