8598eddd2fc159c20a27e5c8996e8303bb297718
seojinsarejj <s19009@naver.com>
2021-03-15 17:39:07 +0900

    [ADD] Add Team model

--- dorandoran/teams/models.py
+++ dorandoran/teams/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model) :
+    team_id = models.AutoField(primary_key=True) 
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=250,null=True)
+    # reserver_id
