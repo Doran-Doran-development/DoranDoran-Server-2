4318e27bea4df802b076c2763a3f70939bf790d6
seojinsarejj <s19009@naver.com>
2021-03-16 08:23:07 +0900

    [CHORE] Apply code formatter

--- dorandoran/teams/models.py
+++ dorandoran/teams/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Team(models.Model) :
-    team_id = models.AutoField(primary_key=True,db_column="team_id") 
+class Team(models.Model):
+    team_id = models.AutoField(primary_key=True, db_column="team_id")
     name = models.CharField(max_length=25)
-    description = models.CharField(max_length=250,null=True)
+    description = models.CharField(max_length=250, null=True)
     reserver_id = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
-class TeamMember(models.Model) :
-    team_member_id = models.AutoField(primary_key=True,db_column="team_member_id")
-    team_id = models.ForeignKey("Team",on_delete=models.CASCADE)
-    member_id = models.ForeignKey("users.User",on_delete=models.CASCADE)
+
+class TeamMember(models.Model):
+    team_member_id = models.AutoField(primary_key=True, db_column="team_member_id")
+    team_id = models.ForeignKey("Team", on_delete=models.CASCADE)
+    member_id = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
     class Meta:
-        unique_together = ('team_id','member_id',)
+        unique_together = (
+            "team_id",
+            "member_id",
+        )
