62aaa44f71baba7cddfebb1a608db995c5f76cf6
icefirebear <woungsub1234@gmail.com>
2021-03-15 16:53:08 +0900

    build: db setting change sqlite to mysql

--- dorandoran/config/settings/local.py
+++ dorandoran/config/settings/local.py
@@ -1,12 +1,16 @@
 from .base import *
-
+import os
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.0", "localhost"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
