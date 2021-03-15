708c57d70d0afc7d1a8c01196743a05519745667
seojinsarejj <s19009@naver.com>
2021-03-15 20:37:18 +0900

    [ADD] Add ForeignKey

--- dorandoran/teams/models.py
+++ dorandoran/teams/models.py
@@ -5,10 +5,10 @@ class Team(models.Model) :
     team_id = models.AutoField(primary_key=True) 
     name = models.CharField(max_length=25)
     description = models.CharField(max_length=250,null=True)
-    # reserver_id
+    reserver_id = models.ForeignKey("users.StudentProfile", on_delete=models.CASCADE)
 
 class TeamMember(models.Model) :
     team_id = models.ForeignKey("Team",on_delete=models.CASCADE)
-    # member_id
+    member_id = models.FOreignKey("users.StutdentProfile",on_delete=models.CASCADE)
     class Meta:
         unique_together = ('team_id','member_id',)
