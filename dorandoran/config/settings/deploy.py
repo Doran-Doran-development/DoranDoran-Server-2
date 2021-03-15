6feca7d7d02a135714841c72de684fb3b75e03ec
hanbin8269 <hanbin8269@gmail.com>
2021-03-15 09:01:29 +0900

    [ADD] deploy DATABASE URL 설정 추가

--- dorandoran/config/settings/deploy.py
+++ dorandoran/config/settings/deploy.py
@@ -1,3 +1,9 @@
+import os
+import dj_database_url
 from .base import *
 
 DEBUG = False
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {"default": dj_database_url.config(default=DATABASE_URL)}
