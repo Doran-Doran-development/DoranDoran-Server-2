bd99ebc64625de85b440a3b818ca4056ab9d0950
변웅섭 <woungsub1234@gmail.com>
2021-03-16 15:50:31 +0900

    feat: Modify status field

--- dorandoran/escape/models.py
+++ dorandoran/escape/models.py
@@ -4,32 +4,21 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
-class Status(models.Model):
-    ACCEPTED = 1
-    DENIED = 2
-    WAITING = 3
-    LATED = 4
-
-    STATUS_CHOICES = {
-        (ACCEPTED, "accepted"),
-        (DENIED, "denied"),
-        (WAITING, "waiting"),
-        (LATED, "lated"),
-    }
-
-    id = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, primary_key=True)
-
-    def __str__(self):
-        return self.get_id_display()
-
-
 class Escape(models.Model):
+    STATUS_CHOICES = {
+        (1, "accepted"),
+        (2, "denied"),
+        (3, "waiting"),
+        (4, "expired"),
+    }
     id = models.AutoField(primary_key=True, db_column="id")
     applyer_id = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, db_column="applyer_id"
     )
     reason = models.CharField(_("reason"), max_length=100, null=False, default="reason")
-    status = models.ManyToManyField(Status)
+    status = models.PositiveSmallIntegerField(
+        choices=STATUS_CHOICES, null=False, default=3
+    )
     start_at = models.DateTimeField(_("start time"))
     end_at = models.DateTimeField(_("end time"))
 
