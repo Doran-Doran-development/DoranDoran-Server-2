c3fe406879a4ea5fdfe778396a549bc3e64cf65a
hanbin8269 <hanbin8269@gmail.com>
2021-03-17 10:10:24 +0900

    chore: Delete Role relation table

--- dorandoran/users/models.py
+++ dorandoran/users/models.py
@@ -4,23 +4,6 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 
-class Role(models.Model):
-    STUDENT = 1
-    TEACHER = 2
-    ADMIN = 3
-
-    ROLE_CHOICES = {
-        (STUDENT, "student"),
-        (TEACHER, "teacher"),
-        (ADMIN, "admin"),
-    }
-
-    id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
-
-    def __str__(self):
-        return self.get_id_display()
-
-
 class User(AbstractUser):
     first_name = None
     last_login = None
@@ -31,15 +14,20 @@ class User(AbstractUser):
     groups = None
     user_permissions = None
 
-    user_id = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, db_column="user_id"
-    )
+    STUDENT = 1
+    TEACHER = 2
+    ADMIN = 3
 
+    ROLE_CHOICES = {
+        (STUDENT, "student"),
+        (TEACHER, "teacher"),
+        (ADMIN, "admin"),
+    }
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, db_column="id")
     name = models.CharField(_("real name"), max_length=150, default="unknown")
-
     email = models.EmailField(_("email address"), unique=True, max_length=128)
-
-    role = models.ManyToManyField(Role)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES)
 
 
 class StudentProfile(models.Model):
