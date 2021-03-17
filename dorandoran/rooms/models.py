56783feda373a427db9143c2f3123a1888ccb3fe
hanbin8269 <hanbin8269@gmail.com>
2021-03-17 10:11:12 +0900

    chore: Delete room_id field

--- dorandoran/rooms/models.py
+++ dorandoran/rooms/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Room(models.Model):
     id = models.AutoField(primary_key=True, db_column="id")
-    room_id = models.AutoField(primary_key=True, db_column="room_id")
     name = models.CharField(max_length=50)
     owner_id = models.ForeignKey(
         "users.User", on_delete=models.SET_NULL, null=True, db_column="owner_id"
