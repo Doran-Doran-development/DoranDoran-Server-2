e46c1240f70bd33dcb617ed32718d434ae3b77f8
hanbin8269 <hanbin8269@gmail.com>
2021-03-17 10:09:01 +0900

    chore: Add id field and Change field name

--- dorandoran/rooms/models.py
+++ dorandoran/rooms/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
 class Room(models.Model):
+    id = models.AutoField(primary_key=True, db_column="id")
     room_id = models.AutoField(primary_key=True, db_column="room_id")
     name = models.CharField(max_length=50)
-    owner_id = models.ForeignKey("users.User", on_delete=models.SET_NULL, null=True)
+    owner_id = models.ForeignKey(
+        "users.User", on_delete=models.SET_NULL, null=True, db_column="owner_id"
+    )
     status = models.BooleanField(default=False)
     capacity = models.IntegerField()
     description = models.TextField(null=True)
