4e1f48cf34764bb72da5c1ea83cb13d0b3d43d56
변웅섭 <woungsub1234@gmail.com>
2021-03-17 16:05:29 +0900

    chore: Add drf_yasg package

--- dorandoran/config/settings/base.py
+++ dorandoran/config/settings/base.py
@@ -14,6 +14,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_yasg",
+    "restframework",
     "escapes",
     "users",
     "rooms",
