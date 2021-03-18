f0db8e56b4546ff2c266bdb9a5ca6a13d83adddf
변웅섭 <woungsub1234@gmail.com>
2021-03-18 10:51:32 +0900

    chore: change restframework to rest_framework

--- dorandoran/config/settings/base.py
+++ dorandoran/config/settings/base.py
@@ -15,7 +15,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "drf_yasg",
-    "restframework",
+    "rest_framework",
     "escapes",
     "users",
     "rooms",
