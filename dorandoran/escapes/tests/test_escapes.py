fca6c3de5d14fafc015861493080d5d05382cd64
seojinsarejj <s19009@naver.com>
2021-03-18 14:48:06 +0900

    feat: Create user data in setup of tests

---
+++ dorandoran/escapes/tests/test_escapes.py
@@ -0,0 +1,35 @@
+from django.test import TestCase, Client
+from rest_framework import status
+from escapes.models import EscapeQueue
+from users.models import User, StudentProfile, TeacherProfile
+from django.contrib.auth.hashers import make_password
+
+import uuid
+import json
+
+client = Client()
+
+
+class ApplyEscape(TestCase):
+    def setUp(self):
+
+        student_id = uuid.uuid4
+
+        user = {
+            "id": student_id,
+            "email": "student1@example.com",
+            "name": "학생",
+            "password": make_password("1234"),
+            "role": 1,
+        }
+
+        User.objects.create(**student)
+
+        student = {
+            "user_id": student_id,
+            "entrance_year": 2019,
+            "classroom": 1,
+            "number": 1,
+        }
+
+        StudentProfile.objects.create(**student)
