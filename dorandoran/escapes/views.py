e0823dfa68c845b1ab3c13bdbecb672159d4a8e4
seojinsarejj <s19009@naver.com>
2021-03-20 00:51:02 +0900

    feat: Add EscapeViewSet create view

--- dorandoran/escapes/views.py
+++ dorandoran/escapes/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework import status
 
-# Create your views here.
+from .serializers import EscapeQueueSerializer
+from .models import EscapeQueue
+
+
+class EscapeViewSet(viewsets.ViewSet):
+    def create(self, request):
+
+        obj = {
+            "applicant_id": request.user.id
+            "reason": request.data["reason"],
+            "start_at": request.data["start_at"],
+            "end_at": request.data["end_at"],
+        }
+        serializer = EscapeQueueSerializer(data=obj)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors)
+
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
