6a823146bff0fba25a655aece9a9737fef7aeb12
hanbin8269 <hanbin8269@gmail.com>
2021-03-19 22:01:01 +0900

    feat: Add teacher api to urls

--- dorandoran/users/urls.py
+++ dorandoran/users/urls.py
@@ -1,3 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from .views import TeacherProfileViewSet
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"teacher", TeacherProfileViewSet)
+
+urlpatterns = [
+    path("/", include(router.urls)),
+]
