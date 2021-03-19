cfaabfe963fb8e9d74bb12f243c04043ee2b13b3
hanbin8269 <hanbin8269@gmail.com>
2021-03-19 22:02:34 +0900

    misc: Apply black formatter

--- dorandoran/escapes/migrations/0001_initial.py
+++ dorandoran/escapes/migrations/0001_initial.py
@@ -9,22 +9,48 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('users', '0001_initial'),
+        ("users", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='EscapeQueue',
+            name="EscapeQueue",
             fields=[
-                ('id', models.AutoField(db_column='id', primary_key=True, serialize=False)),
-                ('reason', models.CharField(default='reason', max_length=100, verbose_name='reason')),
-                ('status', models.PositiveSmallIntegerField(choices=[(1, 'accepted'), (2, 'denied'), (3, 'waiting'), (4, 'expired')], default=3)),
-                ('start_at', models.DateTimeField(verbose_name='start time')),
-                ('end_at', models.DateTimeField(verbose_name='end time')),
-                ('applicant_id', models.ForeignKey(db_column='applicant_id', on_delete=django.db.models.deletion.CASCADE, to='users.user')),
+                (
+                    "id",
+                    models.AutoField(db_column="id", primary_key=True, serialize=False),
+                ),
+                (
+                    "reason",
+                    models.CharField(
+                        default="reason", max_length=100, verbose_name="reason"
+                    ),
+                ),
+                (
+                    "status",
+                    models.PositiveSmallIntegerField(
+                        choices=[
+                            (1, "accepted"),
+                            (2, "denied"),
+                            (3, "waiting"),
+                            (4, "expired"),
+                        ],
+                        default=3,
+                    ),
+                ),
+                ("start_at", models.DateTimeField(verbose_name="start time")),
+                ("end_at", models.DateTimeField(verbose_name="end time")),
+                (
+                    "applicant_id",
+                    models.ForeignKey(
+                        db_column="applicant_id",
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="users.user",
+                    ),
+                ),
             ],
             options={
-                'db_table': 'EscapeQueue',
+                "db_table": "EscapeQueue",
             },
         ),
     ]
