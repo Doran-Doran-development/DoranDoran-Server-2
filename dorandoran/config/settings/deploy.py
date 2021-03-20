9ec2fc1b1c257ae4b4a36d1b0320a5dec8b324c3
변웅섭 <woungsub1234@gmail.com>
2021-03-21 04:05:50 +0900

    fix: Change allowed host

--- dorandoran/config/settings/deploy.py
+++ dorandoran/config/settings/deploy.py
@@ -3,7 +3,7 @@ import dj_database_url
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["0.0.0.0"]
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {"default": dj_database_url.config(default=DATABASE_URL)}
