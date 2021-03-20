6e88167a1a782ced08a0f47185e53d466a1bba24
hanbin8269 <hanbin8269@gmail.com>
2021-03-20 22:42:06 +0900

    feat: Add student viewset to urls

--- dorandoran/users/urls.py
+++ dorandoran/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import TeacherProfileViewSet
+from .views import TeacherProfileViewSet, StudentProfileViewSet
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r"teacher", TeacherProfileViewSet)
+router.register(r"student", StudentProfileViewSet)
 
 urlpatterns = [
     path("/", include(router.urls)),
