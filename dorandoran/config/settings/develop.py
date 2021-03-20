bfde200cba7805bd0484946e51f9e10795058bf3
변웅섭 <woungsub1234@gmail.com>
2021-03-21 04:06:18 +0900

    chore: Add allowed host on develop

--- dorandoran/config/settings/develop.py
+++ dorandoran/config/settings/develop.py
@@ -3,7 +3,7 @@ import dj_database_url
 from .base import *
 
 DEBUG = True
-
+ALLOWED_HOSTS = ["*"]
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {"default": dj_database_url.config(default=DATABASE_URL)}
