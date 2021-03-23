e601bc609f123abc13555d31081eab33863882b6
hanbin8269 <hanbin8269@gmail.com>
2021-03-23 20:55:24 +0900

    feat: Add ObtainTokenView(Login)

--- dorandoran/auth/views.py
+++ dorandoran/auth/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ObtainTokenSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ObtainTokenView(generics.GenericAPIView):
+    serializer_class = ObtainTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if not serializer.is_valid(raise_exception=True):
+            return Response(serializer.errors, status=400)
+
+        response_data = serializer.validated_data
+        response = Response(response_data)
+        return response
