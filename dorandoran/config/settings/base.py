38a7bdc02ef9c6118786ce20ff51a0554a6df5b9
변웅섭 <woungsub1234@gmail.com>
2021-03-24 21:10:38 +0900

    chore: Add s3 setting

--- dorandoran/config/settings/base.py
+++ dorandoran/config/settings/base.py
@@ -112,7 +112,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_CUSTOM_DOMAIN = os.getenv("AWS_S3_CUSTOM_DOMAIN")
-AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=" + os.getenv("CacheControl")}
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86000"}
 AWS_DEFAULT_ACL = ""
 AWS_LOCATION = os.getenv("AWS_LOCATION")
 
