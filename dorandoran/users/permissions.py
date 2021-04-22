b3683286d965656c2837297a4feb937525b9fb38
hanbin8269 <hanbin8269@gmail.com>
2021-04-22 09:45:53 +0900

    feat: IsTeacher permission 모듈 추가

--- dorandoran/users/permissions.py
+++ dorandoran/users/permissions.py
@@ -7,3 +7,10 @@ class IsOwnerOrAdmin(BasePermission):
         if request.user.is_anonymous:
             return False
         return str(request.user.id) == str(request.parser_context["kwargs"]["id"])
+
+
+class IsTeacher(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
+        return request.user.role == UserRole.TEACHER
