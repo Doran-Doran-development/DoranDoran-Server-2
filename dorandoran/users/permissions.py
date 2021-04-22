249710fb279216d249fcd27572b4f528fbda618c
hanbin8269 <hanbin8269@gmail.com>
2021-04-22 09:45:23 +0900

    feat: IsOwnerOrAdmin permission 모듈 추가

---
+++ dorandoran/users/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+from .enums import UserRole
+
+
+class IsOwnerOrAdmin(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
+        return str(request.user.id) == str(request.parser_context["kwargs"]["id"])
