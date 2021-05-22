0bd0b522fa76218bdb1d60f6ea8b22ac8f5b0b08
hanbin8269 <hanbin8269@gmail.com>
2021-05-22 22:25:12 +0900

    feat : Add verification mail sending process

--- dorandoran/users/views.py
+++ dorandoran/users/views.py
@@ -1,5 +1,10 @@
 from rest_framework import mixins, viewsets
-
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from core.email_verification import account_activation_token
 from .models import TeacherProfile, StudentProfile
 from .serializers import (
     TeacherProfileSerializer,
@@ -29,3 +34,25 @@ class StudentProfileViewSet(
     queryset = StudentProfile.objects.select_related("user").all()
     serializer_class = StudentProfileSerializer
     lookup_field = "user_id"
+
+    def perform_create(self, serializer):
+        user_instance = serializer.save()
+        current_site = get_current_site(self.request)
+
+        message = render_to_string(
+            "email/email-verification.html",
+            {
+                "user": user_instance.user,
+                "domain": current_site.domain,
+                "uid": urlsafe_base64_encode(force_bytes(user_instance.user.pk)),
+                "token": account_activation_token.make_token(user_instance.user),
+            },
+        )
+
+        send_mail(
+            subject="Activate your DoranDoran account.",
+            message=message,
+            from_email=None,
+            recipient_list=[user_instance.user.email],
+            fail_silently=False,
+        )
