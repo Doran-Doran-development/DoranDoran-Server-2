47da085033c7f5dc5f541f609a8ea487c102322b
hanbin8269 <hanbin8269@gmail.com>
2021-05-22 20:20:26 +0900

    chore: Rename auth app to accounts in url

--- dorandoran/config/urls.py
+++ dorandoran/config/urls.py
@@ -22,7 +22,7 @@ from rest_framework import permissions
 
 
 schema_url_patterns = [
-    path("auth", include("auth.urls")),
+    path("accounts", include("accounts.urls")),
     path("users", include("users.urls")),
     path("reservations", include("reservations.urls")),
     path("rooms", include("rooms.urls")),
