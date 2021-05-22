2fe3e823cb25d3b2bd287a1011f616d5529c4736
hanbin8269 <hanbin8269@gmail.com>
2021-05-22 22:26:43 +0900

    chore : delete email_verification_message_generator function

--- dorandoran/core/email_verification.py
+++ dorandoran/core/email_verification.py
@@ -1,18 +1,13 @@
-from datetime import time
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six
 from django.contrib.auth import get_user_model
+
 
 UserModel = get_user_model()
 
 
-class TokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user: UserModel, timestamp: int) -> str:
-        return (
-            six.text_type(user.id)
-            + six.text_type(timestamp)
-            + six.text_type(user.is_active)
-        )
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (str(user.pk) + str(timestamp)) + str(user.is_active)
 
 
-account_activation_token = TokenGenerator()
+account_activation_token = AccountActivationTokenGenerator()
