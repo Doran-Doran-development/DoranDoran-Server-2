b684a2f0fab0282b53afcba392016dee65e1ba99
hanbin8269 <hanbin8269@gmail.com>
2021-05-22 20:10:10 +0900

    feat : Create ActivateAccountView template

--- dorandoran/auth/views.py
+++ dorandoran/auth/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
+from rest_framework.views import APIView
 from .serializers import ObtainTokenSerializer
 from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
 
 
 class ObtainTokenView(generics.GenericAPIView):
@@ -14,3 +18,8 @@ class ObtainTokenView(generics.GenericAPIView):
         response_data = serializer.validated_data
         response = Response(response_data)
         return response
+
+
+class ActivateAccountView(APIView):
+    def get(self, request, uid64, token):
+        pass
