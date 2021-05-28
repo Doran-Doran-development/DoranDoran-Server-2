61faad246d1b3e716a6e1e114f821bb236d0fb57
hanbin8269 <hanbin8269@gmail.com>
2021-05-28 16:15:06 +0900

    feat : Create BaseUserAPITest abstact class

--- dorandoran/users/tests/test_apis.py
+++ dorandoran/users/tests/test_apis.py
@@ -1,22 +1,37 @@
-from django.contrib.auth import get_user_model
-from django.urls import reverse
-from django.test import TestCase
-from rest_framework import status
-from rest_framework.test import APIClient
+from django.test import Client
+from model_mommy import mommy
+from users.enums import UserRole
+from accounts.utils import jwt_encode_handler, jwt_payload_handler
+import abc
 
 
-class UserAPITest(TestCase):
+class BaseUserAPITest:
     def setUp(self):
-        self.client = APIClient()
+        self.client = Client()
+        self.fixture_student_user = mommy.make(
+            "users.User", role=UserRole.STUDENT.value, is_active=True
+        )
+        self.fixture_profile = self.create_profile(self.fixture_student_user.id)
 
-    def test_create_teacher_successful(self):
-        # given
-        payload = {
-            "user": {"email": "teacher1@example.com", "password": "1234"},
-            "grade": 1,
-            "classroom": 1,
-        }
-        # when
-        response = self.client.post("/users/teacher", payload, format="json")
-        # then
-        self.assertEqual(response.status_code, 201)
+        payload = jwt_payload_handler(self.fixture_student_user)
+        self.token = jwt_encode_handler(payload)
+
+    @abc.abstractclassmethod
+    def create_profile(self, user_id):
+        pass
+
+    @abc.abstractclassmethod
+    def test_create_user_success(self):
+        pass
+
+    @abc.abstractclassmethod
+    def test_retrieve_user_success(self):
+        pass
+
+    @abc.abstractclassmethod
+    def test_list_user_success(self):
+        pass
+
+    @abc.abstractclassmethod
+    def test_delete_user_success(self):
+        pass
