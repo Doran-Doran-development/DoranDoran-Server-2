0918f2cc5aea498a690d2cbcfbde4f5e4c20eec2
hanbin8269 <hanbin8269@gmail.com>
2021-05-28 16:15:47 +0900

    feat : Create C R D API Test

--- dorandoran/users/tests/test_apis.py
+++ dorandoran/users/tests/test_apis.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+from rest_framework import status
 from django.test import Client
 from model_mommy import mommy
 from users.enums import UserRole
@@ -35,3 +39,60 @@ class BaseUserAPITest:
     @abc.abstractclassmethod
     def test_delete_user_success(self):
         pass
+
+
+class StudentAPITest(BaseUserAPITest, TestCase):
+    def create_profile(self, user_id):
+        return mommy.make("users.StudentProfile", user_id=user_id)
+
+    def test_create_user_success(self):
+        # given
+        payload = {
+            "user": {
+                "email": "student1@example.com",
+                "password": "1234",
+                "name": "student1",
+            },
+            "entrance_year": 2021,
+            "grade": 1,
+            "classroom": 1,
+            "number": 1,
+        }
+        # when
+        response = self.client.post(
+            "/users/student",
+            data=payload,
+            content_type="application/json",
+        )
+        # then
+        self.assertEqual(response.status_code, 201)
+
+    def test_retrieve_user_success(self):
+        # when
+        response = self.client.get(
+            f"/users/student/{self.fixture_profile.user_id}",
+            content_type="application/json",
+        )
+        # then
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_user_successful(self):
+        # when
+        response = self.client.get(
+            "/users/student",
+            content_type="application/json",
+        )
+        # then
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_user_successful(self):
+        # given
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.token}
+        # when
+        response = self.client.delete(
+            f"/users/student/{self.fixture_profile.user_id}",
+            **header,
+            content_type="application/json",
+        )
+        # then
+        self.assertEqual(response.status_code, 204)
