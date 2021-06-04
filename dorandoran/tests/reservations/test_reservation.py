7f8aa1529aeb7ecdb75be2942a5a0b0d88c0d2bd
변웅섭 <woungsub1234@gmail.com>
2021-06-04 22:50:59 +0900

    test: Separate fixture_user into student and teacher

--- dorandoran/tests/reservations/test_reservation.py
+++ dorandoran/tests/reservations/test_reservation.py
@@ -7,10 +7,10 @@ from tests.reservations.base import BaseReservationAPITest
 
 class ReservationAPITest(BaseReservationAPITest, TestCase):
     def prepare_fixture(self):
-        self.fixture_user = mommy.make(
+        self.fixture_user_student = mommy.make(
             "users.User", role=UserRole.STUDENT.value, is_active=True
         )
-        self.fixture_teacher = mommy.make(
+        self.fixture_user_teacher = mommy.make(
             "users.User", role=UserRole.TEACHER.value, is_active=True
         )
         self.fixture_team = mommy.make("teams.Team", applicant_id=self.fixture_user)
@@ -26,11 +26,14 @@ class ReservationAPITest(BaseReservationAPITest, TestCase):
             team_id=self.fixture_team,
         )
 
-        payload = jwt_payload_handler(self.fixture_user)
-        self.valid_token = jwt_encode_handler(payload)
+        student_payload = jwt_payload_handler(self.fixture_user_teacher)
+        teacher_payload = jwt_payload_handler(self.fixture_user_student)
+        self.student_valid_token = jwt_encode_handler(student_payload)
+        self.teacher_valid_token = jwt_encode_handler(teacher_payload)
 
     def test_create_reservation_success(self):
         # given
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.student_valid_token}
         payload = {
             "time": [8, 9, 10, 11],
             "description": "2021주소창대회참여를 위해 홈베이스[1]실을 대여합니다",
@@ -39,6 +42,7 @@ class ReservationAPITest(BaseReservationAPITest, TestCase):
         # when
         response = self.client.post(
             "/reservations/{}".format(self.fixture_room.id),
+            **header,
             data=payload,
             content_type="application/json",
         )
@@ -47,7 +51,7 @@ class ReservationAPITest(BaseReservationAPITest, TestCase):
 
     def test_retrieve_reservation_success(self):
         # given
-        header = {"HTTP_AUTHORIZATION": "jwt " + self.valid_token}
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.student_valid_token}
         # when
         response = self.client.get(
             "/reservations/my",
@@ -59,7 +63,7 @@ class ReservationAPITest(BaseReservationAPITest, TestCase):
 
     def test_list_reservation_success(self):
         # given
-        header = {"HTTP_AUTHORIZATION": "jwt " + self.valid_token}
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.student_valid_token}
         # when
         response = self.client.get(
             "/reservations?room_id={}".format(self.fixture_room.id),
@@ -69,7 +73,7 @@ class ReservationAPITest(BaseReservationAPITest, TestCase):
 
     def test_accept_reservation_success(self):
         # given
-        header = {"HTTP_AUTHORIZATION": "jwt " + self.valid_token}
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.teacher_valid_token}
         # when
         response = self.client.patch(
             "/reservations/{}/accept".format(self.fixture_reservation.id),
@@ -81,7 +85,7 @@ class ReservationAPITest(BaseReservationAPITest, TestCase):
 
     def test_deny_reservation_success(self):
         # given
-        header = {"HTTP_AUTHORIZATION": "jwt " + self.valid_token}
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.teacher_valid_token}
         # when
         response = self.client.patch(
             "/reservations/{}/deny".format(self.fixture_reservation.id),
@@ -93,7 +97,7 @@ class ReservationAPITest(BaseReservationAPITest, TestCase):
 
     def test_cancel_reservation_success(self):
         # given
-        header = {"HTTP_AUTHORIZATION": "jwt " + self.valid_token}
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.teacher_valid_token}
 
         # when
         response = self.client.delete(
