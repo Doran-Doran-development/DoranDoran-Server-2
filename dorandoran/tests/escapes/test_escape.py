e21ef56c81dbe9d125b4e4b892ee9b5d8eb36968
seojinsarejj <s19009@naver.com>
2021-06-05 20:32:31 +0900

    feat: create test_list_escapes_success

--- dorandoran/tests/escapes/test_escape.py
+++ dorandoran/tests/escapes/test_escape.py
@@ -1,3 +1,6 @@
+from django.test import Client
+import abc
+
 from django.test import TestCase
 from model_mommy import mommy
 from users.enums import UserRole
@@ -5,16 +8,26 @@ from accounts.utils import jwt_encode_handler, jwt_payload_handler
 
 
 class EscapeAPITest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.prepare_fixture()
+
+    @abc.abstractmethod
     def prepare_fixture(self):
         self.fixture_user = mommy.make(
             "users.User", role=UserRole.STUDENT.value, is_active=True
         )
         self.fixture_profile = mommy.make(
-            "users.StudentProfile", applicant_id=self.fixture_user.id
+            "users.StudentProfile", user_id=self.fixture_user.id
         )
         payload = jwt_payload_handler(self.fixture_user)
         self.valid_token = jwt_encode_handler(payload)
 
+        self.fixture_escape = mommy.make(
+            "escapes.EscapeQueue", applicant_id=self.fixture_user
+        )
+
+    @abc.abstractmethod
     def test_create_escapes_success(self):
         # given
         header = {"HTTP_AUTHORIZATION": "jwt " + self.valid_token}
@@ -33,3 +46,16 @@ class EscapeAPITest(TestCase):
         )
         # then
         self.assertEqual(response.status_code, 201)
+
+    @abc.abstractmethod
+    def test_list_escapes_success(self):
+
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.valid_token}
+
+        response = self.client.get(
+            "/escapes/",
+            **header,
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 200)
