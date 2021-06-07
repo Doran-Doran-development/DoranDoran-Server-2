b3b8b85672e6514cdbe075e898e4e930bbbac323
seojinsarejj <s19009@naver.com>
2021-06-07 14:50:13 +0900

    feat: create escape accept and deny test

--- dorandoran/tests/escapes/test_escape.py
+++ dorandoran/tests/escapes/test_escape.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.test import Client
 import abc
 
@@ -14,23 +15,37 @@ class EscapeAPITest(TestCase):
 
     @abc.abstractmethod
     def prepare_fixture(self):
-        self.fixture_user = mommy.make(
+
+        # student
+        self.student_user = mommy.make(
             "users.User", role=UserRole.STUDENT.value, is_active=True
         )
-        self.fixture_profile = mommy.make(
-            "users.StudentProfile", user_id=self.fixture_user.id
+        self.student_profile = mommy.make(
+            "users.StudentProfile", user_id=self.student_user.id
         )
-        payload = jwt_payload_handler(self.fixture_user)
-        self.valid_token = jwt_encode_handler(payload)
+        payload = jwt_payload_handler(self.student_user)
+        self.student_token = jwt_encode_handler(payload)
 
+        # teacher
+        self.teacher_user = mommy.make(
+            "users.User", role=UserRole.TEACHER.value, is_active=True
+        )
+        self.teacher_profile = mommy.make(
+            "users.TeacherProfile", user_id=self.teacher_user.id
+        )
+        self.fixture_certification_code = mommy.make("users.TeacherCertificationCode")
+        payload = jwt_payload_handler(self.teacher_user)
+        self.teacher_token = jwt_encode_handler(payload)
+
+        # escape
         self.fixture_escape = mommy.make(
-            "escapes.EscapeQueue", applicant_id=self.fixture_user
+            "escapes.EscapeQueue", applicant_id=self.student_user
         )
 
     @abc.abstractmethod
     def test_create_escapes_success(self):
         # given
-        header = {"HTTP_AUTHORIZATION": "jwt " + self.valid_token}
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.student_token}
 
         payload = {
             "reason": "Example reason",
@@ -50,10 +65,36 @@ class EscapeAPITest(TestCase):
     @abc.abstractmethod
     def test_list_escapes_success(self):
 
-        header = {"HTTP_AUTHORIZATION": "jwt " + self.valid_token}
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.student_token}
 
         response = self.client.get(
             "/escapes/",
+            **header,
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    @abc.abstractmethod
+    def test_accept_escapes_success(self):
+
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.teacher_token}
+
+        response = self.client.patch(
+            "/escapes/{self.fixture_escape.id}/accept",
+            **header,
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    @abc.abstractmethod
+    def test_deny_escapes_success(self):
+
+        header = {"HTTP_AUTHORIZATION": "jwt " + self.teacher_token}
+
+        response = self.client.patch(
+            "/escapes/{self.fixture_escape.id}/deny",
             **header,
             content_type="application/json",
         )
